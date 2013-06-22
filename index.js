d87cfa314a5dfd794b9a24694867ee95b40b8a78
seca <levandovskiy.s@gmail.com>
2013-06-22 11:00:10 +0300

    fix

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-module.exports = require('./lib/');
+require('coffee-script');
+module.exports = {
+    Schema: require('./lib/schema'),
+    Model: require('./lib/model'),
+    Collection: require('./lib/collection'),
+    DataProvider: require('./lib/data-provider'),
+    MysqlProxy: require('./lib/mysql/proxy'),
+    MysqlQueryBuilder: require('./lib/mysql/query-builder'),
+    Validator: require('./lib/validator')
+};
